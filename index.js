d47107663463c2b70cc34ba645e7a1da00b17f78
Adinata Ekwan <134189355+Starck27@users.noreply.github.com>
2024-09-23 06:51:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const monthNameElement = document.getElementById("month-name");
+const dayNameElement = document.getElementById("day-name");
+const dayNumberElement = document.getElementById("day-number");
+const yearElement = document.getElementById("year");
+const languageElement = document.getElementById("language");
+
+const defaultLanguage = "en";
+
+function updatedCalender(language) {
+  const date = new Date();
+  monthNameElement.innerText = date.toLocaleString(language, {
+    month: "long",
+  });
+  dayNameElement.innerText = date.toLocaleString(language, {
+    weekday: "long",
+  });
+  dayNumberElement.innerText = date.toLocaleString(language, {
+    day: "2-digit",
+  });
+  yearElement.innerText = date.toLocaleString(language, {
+    year: "numeric",
+  });
+}
+
+updatedCalender(defaultLanguage);
+
+languageElement.addEventListener("change", (e) => {
+  const selectedLanguage = e.target.value;
+  updatedCalender(selectedLanguage);
+});
